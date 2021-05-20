e3d071544e5cf9feadb21eacab7b4f968e4e6ce0
Roger Becerra <rogelio.becerra001@gmail.com>
2021-05-20 09:32:25 -0500

    single make componeent

--- angular-dealership-frontend/src/app/app.module.ts
+++ angular-dealership-frontend/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { LoginComponent } from './login/login.component';
 import { MakesComponent } from './makes/makes.component';
 import { ModelsComponent } from "./models/models.component";
 import { LogoutComponent } from './logout/logout.component';
+import { MakeComponent } from './make/make.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { LogoutComponent } from './logout/logout.component';
     LoginComponent,
     MakesComponent,
     ModelsComponent,
-    LogoutComponent
+    LogoutComponent,
+    MakeComponent
   ],
   imports: [
     BrowserModule,
