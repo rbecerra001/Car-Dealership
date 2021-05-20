d717010c3b6dad2e0f9743454262f23f85547b65
Roger Becerra <rogelio.becerra001@gmail.com>
2021-05-20 07:21:37 -0500

    app models

--- angular-dealership-frontend/src/app/app.module.ts
+++ angular-dealership-frontend/src/app/app.module.ts
@@ -1,26 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
-import { MakeComponent } from './make/make.component';
+import { SignupComponent } from "./signup/signup.component";
+import { LoginComponent } from './login/login.component';
 import { MakesComponent } from './makes/makes.component';
-import { ModelsComponent } from './models/models.component';
+import { ModelsComponent } from "./models/models.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     SignupComponent,
     LoginComponent,
-    HomeComponent,
-    MakeComponent,
     MakesComponent,
     ModelsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
