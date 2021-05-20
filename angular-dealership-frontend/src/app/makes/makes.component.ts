6f73ca8b97ae9c67bc2daf225d67df9609d4229d
Roger Becerra <rogelio.becerra001@gmail.com>
2021-05-20 08:44:48 -0500

    makes components html

--- angular-dealership-frontend/src/app/makes/makes.component.ts
+++ angular-dealership-frontend/src/app/makes/makes.component.ts
@@ -1,15 +1,29 @@
+import { CategoryService } from 'src/app/services/make/make.service';
 import { Component, OnInit } from '@angular/core';
+declare const M;
 
 @Component({
   selector: 'app-makes',
-  templateUrl: './makes.component.html',
+  templateUrl: './makes.makes.html',
   styleUrls: ['./makes.component.css']
 })
 export class MakesComponent implements OnInit {
+  public makes: [];
 
-  constructor() { }
+  constructor(private makeService: MakeService) { }
 
-  ngOnInit(): void {
+  getMakes(): any {
+    this.makeService.getMakes().subscribe(response => {
+      this.makes = response;
+    }, err => console.log(err));
   }
 
+  ngOnInit(): void {
+    this.getMakes();
+
+    if (!localStorage.getItem('currentUser')) {
+      const toastHTML = '<span>You must login to see your makes</span>';
+      M.toast({html: toastHTML});
+    }
+  }
 }
--- angular-dealership-frontend/src/app/services/make/make.service.ts
+++ angular-dealership-frontend/src/app/services/make/make.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
+
+const herokuUrl = 'https://damp-bayou-38809.herokuapp.com';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class MakeService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCategories(): any {
+    const token = localStorage.getItem('token');
+    const requestOptions = {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${token}`
+      }),
+    };
+    return this.http
+      .get(`${herokuUrl}/api/makes`, requestOptions);
+  }
 }
